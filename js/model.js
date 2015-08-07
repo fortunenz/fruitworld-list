eff060d7dd130715ef413a6cfd12cac3efd50328
Brian Chen <bchen.chen93@gmail.com>
2015-08-07 13:14:45 +1200

    chore: Added all fruit world branches and installed jquery

--- js/model.js
+++ js/model.js
@@ -2,6 +2,87 @@ var model = {
   "shops": [
     {
       "name": "Fruit World Henderson"
+    },
+    {
+      "name": "Fruit World Pakuranga"
+    },
+    {
+      "name": "Fruit World New Lynn"
+    },
+    {
+      "name": "Fruit World Glen Innes"
+    },
+    {
+      "name": "Fruit World Silverdale"
+    },
+    {
+      "name": "Fruit World Mt Albert"
+    },
+    {
+      "name": "Fruit World Ponsonby"
+    },
+    {
+      "name": "Fruit World Pukekohe"
+    },
+    {
+      "name": "Fruit World Albany"
+    },
+    {
+      "name": "Fruit World Botany"
+    },
+    {
+      "name": "Fruit World Dannemora"
+    },
+    {
+      "name": "Fruit World Mt Roskill"
+    },
+    {
+      "name": "Fruit World Chatswood"
+    },
+    {
+      "name": "Fruit World Takanini"
+    },
+    {
+      "name": "Fruit World Howick"
+    },
+    {
+      "name": "Fruit World Greenlane"
+    },
+    {
+      "name": "Fruit World Pioneer Plaza"
+    },
+    {
+      "name": "Fruit World Mt Wellington"
+    },
+    {
+      "name": "Fruit World Whangaporaoa"
+    },
+    {
+      "name": "Fruit World Hobsonville"
+    },
+    {
+      "name": "Fruit World Hamilton"
+    },
+    {
+      "name": "Fruit World Rosedale"
+    },
+    {
+      "name": "Fruit World Rosedale Albany"
+    },
+    {
+      "name": "Supa Fruit Mart 1"
+    },
+    {
+      "name": "Supa Fruit Mart 2"
+    },
+    {
+      "name": "Supa Fruit Mart 3"
+    },
+    {
+      "name": "Supa Fruit Mart 4"
+    },
+    {
+      "name": "Supa Fruit Mart 5"
     }
   ]
 }
