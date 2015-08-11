a4df72e81f161416bd109e69632e7a13e0e60725
Brian Chen <bchen.chen93@gmail.com>
2015-08-11 21:40:17 +1200

    feat: Added separation of concerns. Adding a new item will enable it to show in the printed format

--- js/buildTable.js
+++ js/buildTable.js
@@ -12,108 +12,7 @@ var buildTable = function(spreadsheetArray) {
   table += "<th>Total</th>"
   table += "</tr>";
 
-  tempTotal = 0;
-  for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-    tempTotal += spreadsheetArray[i].attributes.FW001;
-  }
-
-  if (tempTotal > 0) {
-    table += "<tr><th>Fruit World singlet bag (Large)</th>";
-    for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-      table += "<td>";
-      if (spreadsheetArray[i].attributes.FW001 > 0) {
-        table += spreadsheetArray[i].attributes.FW001;
-      }
-      table += "</td>";
-    }
-    table += "<td>" + tempTotal + "</td>"
-    table += "</tr>";
-  }
-
-  tempTotal = 0;
-  for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-    tempTotal += spreadsheetArray[i].attributes.FW002;
-  }
-  if (tempTotal > 0) {
-    table += "<tr><th>Fruit World singlet bag (Small)</th>";
-    for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-      table += "<td>";
-      if (spreadsheetArray[i].attributes.FW002 > 0) {
-        table += spreadsheetArray[i].attributes.FW002;
-      }
-      table += "</td>";
-    }
-    table += "<td>" + tempTotal + "</td>"
-    table += "</tr>";
-  }
-
-  tempTotal = 0;
-  for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-    tempTotal += spreadsheetArray[i].attributes.FW003;
-  }
-  if (tempTotal > 0) {
-    table += "<tr><th>Fruit World roll bag (300x450mm) 3kg</th>";
-    for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-      table += "<td>";
-      if (spreadsheetArray[i].attributes.FW003 > 0) {
-        table += spreadsheetArray[i].attributes.FW003;
-      }
-      table += "</td>";
-    }
-    table += "<td>" + tempTotal + "</td>"
-    table += "</tr>";
-  }
-
-  tempTotal = 0;
-  for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-    tempTotal += spreadsheetArray[i].attributes.PRB01;
-  }
-  if (tempTotal > 0) {
-    table += "<tr><th>Produce bag (150x300mm) 0.5kg</th>";
-    for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-      table += "<td>";
-      if (spreadsheetArray[i].attributes.PRB01 > 0) {
-        table += spreadsheetArray[i].attributes.PRB01;
-      }
-      table += "</td>";
-    }
-    table += "<td>" + tempTotal + "</td>"
-    table += "</tr>";
-  }
-
-  tempTotal = 0;
-  for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-    tempTotal += spreadsheetArray[i].attributes.ROLL05;
-  }
-  if (tempTotal > 0) {
-    table += "<tr><th>Roll bag (300x450mm) 3kg</th>";
-    for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-      table += "<td>";
-      if (spreadsheetArray[i].attributes.ROLL05 > 0) {
-        table += spreadsheetArray[i].attributes.ROLL05;
-      }
-      table += "</td>";
-    }
-    table += "<td>" + tempTotal + "</td>"
-    table += "</tr>";
-  }
-
-  tempTotal = 0;
-  for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-    tempTotal += spreadsheetArray[i].attributes.SINGN_M;
-  }
-  if (tempTotal > 0) {
-    table += "<tr><th>Natural singlet bag (Medium)</th>";
-    for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-      table += "<td>";
-      if (spreadsheetArray[i].attributes.SINGN_M > 0) {
-        table += spreadsheetArray[i].attributes.SINGN_M;
-      }
-      table += "</td>";
-    }
-    table += "<td>" + tempTotal + "</td>"
-    table += "</tr>";
-  }
+  table += buildRow(spreadsheetArray);
 
   table += "</table>";
 
@@ -123,25 +22,31 @@ var buildTable = function(spreadsheetArray) {
 
 }
 
-// Future feature to work on to add separtion of concern
-var buildRow = function() {
-  var tempTotal = 0;
+// Builds all the rows
+var buildRow = function(spreadsheetArray) {
+  var table = "";
+  var tempTotal;
   var items = model.items;
 
-  for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-    tempTotal += spreadsheetArray[i].attributes.FW001;
+  for (k = 0; k < items.length; k++) {
+    tempTotal = 0;
+    for (i = 0, len = spreadsheetArray.length; i < len; i++) {
+      tempTotal += spreadsheetArray[i].attributes[items[k].code];
+    }
+
+    if (tempTotal > 0) {
+      table += "<tr><th>" + items[k].desription + "</th>";
+      for (i = 0, len = spreadsheetArray.length; i < len; i++) {
+        table += "<td>";
+        if (spreadsheetArray[i].attributes[items[k].code] > 0) {
+          table += spreadsheetArray[i].attributes[items[k].code];
+        }
+        table += "</td>";
+      }
+      table += "<td>" + tempTotal + "</td>"
+      table += "</tr>";
+    }
   }
 
-  if (tempTotal > 0) {
-    table += "<tr><th>Fruit World singlet bag (Large)</th>";
-    for (i = 0, len = spreadsheetArray.length; i < len; i++) {
-      table += "<td>";
-      if (spreadsheetArray[i].attributes.FW001 > 0) {
-        table += spreadsheetArray[i].attributes.FW001;
-      }
-      table += "</td>";
-    }
-    table += "<td>" + tempTotal + "</td>"
-    table += "</tr>";
-  }
+  return table;
 }
